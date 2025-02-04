fcf71b898453d78a2024a35241a3710108b5fb0e
Sudhanshu-06 <sudhanshu767778@gmail.com>
2025-02-04 21:47:37 +0530

    done

---
+++ 03_JavaScript/Day_15/script.js
@@ -0,0 +1,12 @@
+// for of loop
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let value of arr) {
+  console.log(value);
+}
+
+let str= "Rohit is good boy";
+for(let value of str) {
+    console.log(value);
+}
